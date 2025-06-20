963a622335f7af993ef09341e4b2dd5681f4fcb1
Saurabh D <saurabh.dikshit@puppet.com>
2025-06-20 10:04:25 +0100

    function

--- functions/my_custom_function.pp
+++ functions/my_custom_function.pp
@@ -1,5 +1,5 @@
 # See https://puppet.com/docs/puppet/latest/lang_write_functions_in_puppet.html
 # for more information on native puppet functions.
-function testfunc::my_custom_function(Integer $value) >> Integer {
-  $value * 2
+function testfunc::my_custom_function(String $arg) {
+  notice("Function received: ${arg}")
 }
